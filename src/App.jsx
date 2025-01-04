525bca18d810c967d79dd8b7003b5dec30c5e687
jyefuriii <jeffrey.t.fabella@gmail.com>
2025-01-04 22:18:57 +0800

    Implement Navbar and About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-orange-500 font-bold">
-        Welcome to Awwwards
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
     </main>
   );
 };
