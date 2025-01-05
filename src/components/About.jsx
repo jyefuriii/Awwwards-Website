fc8250f3121a7cda894c52d0f1ebd147b7142f63
jyefuriii <jeffrey.t.fabella@gmail.com>
2025-01-05 11:59:55 +0800

    Implement Features, Story & Footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <h2 className="font-general text-sm uppercase md:text-[15px]">
           Welcome to Zentry
         </h2>
 
